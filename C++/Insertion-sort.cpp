5530aaa13c8e9464ba5b93b68782b220a93bf486
Epic-legend128 <100092577+Epic-legend128@users.noreply.github.com>
2022-02-20 23:17:26 +0200

    Update Insertion-sort.cpp

--- C++/Insertion-sort.cpp
+++ C++/Insertion-sort.cpp
@@ -1,3 +1,7 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
 //insertion sort O(n^2) time notation but at the best cases it can reach O^n
 void insertion(vector<int>& list, int limit) {
     for (int i = 1; i<limit; i++) {
@@ -7,4 +11,23 @@ void insertion(vector<int>& list, int limit) {
             swap(list[j+1], list[j]);
         }
     }
+}
+
+int main() {
+    int size = 5;
+    vector<int> nums(size, 0);
+    cout << "Enter "<<size<<" numbers:\n";
+    
+    for (int i = 0; i < size; i++) {
+        int current;
+        cin >> current;
+        nums.push_back(current);
+    }
+    
+    insertion(nums);
+    
+    for (int i = 0; i<size; i++) {
+        cout << nums[i]<<" ";
+    }
+    cout << "\n";
 }
