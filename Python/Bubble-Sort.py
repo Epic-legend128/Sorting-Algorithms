1450b748436778227141bec6d326f9eefebd866c
Epic-legend128 <100092577+Epic-legend128@users.noreply.github.com>
2022-02-21 22:26:39 +0200

    Update Bubble-Sort.py

--- Python/Bubble-Sort.py
+++ Python/Bubble-Sort.py
@@ -1,1 +1,15 @@
-#in progress
+#bubble sort
+def bubble(array, limit):
+  for i in range(limit, 0, -1):
+    for j in range(0, i-1):
+      if array[j] > array[j+1]:
+        temp = array[j]
+        array[j] = array[j+1]
+        array[j+1] = temp
+
+
+#an example
+myArray = [0, 2, 1, -100, 0.2, -3.9, 2398, 382]
+print(myArray) #array before sorted
+bubble(myArray, len(myArray))
+print(myArray) #array after sorted
