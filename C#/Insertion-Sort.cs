5f9efff3dce4cb754ba53c3c656be9d69902f06a
Epic-legend128 <100092577+Epic-legend128@users.noreply.github.com>
2022-12-03 11:05:47 +0100

    Create Insertion-Sort.cs

---
+++ C#/Insertion-Sort.cs
@@ -0,0 +1,29 @@
+using System;
+
+namespace InsertionSort {
+    public class Program {
+        public static void Insertion(ref int[] array) {
+            for (int i = 1; i < array.Length; i++) {
+                int value = array[i];
+                for (int j = i-1; j>=0 && array[j] > value; j--) {
+                    int temp = array[j];
+                    array[j] = array[j+1];
+                    array[j+1] = temp;
+                }
+            }
+        }
+        
+        static void Main(string[] args) {
+            int[] array = new int[] {4, 2, 7, 7, 5, 27, 0, 2, 10, -14, 546, 2};
+            for (int i = 0; i<array.Length; i++) {
+            	Console.Write(array[i]+" ");
+            }
+            Console.WriteLine(" <- Before");
+            Insertion(ref array); // pass by reference to work
+            for (int i = 0; i<array.Length; i++) {
+                Console.Write(array[i]+" ");
+            }
+            Console.WriteLine(" <- After");
+        }
+    }
+}
